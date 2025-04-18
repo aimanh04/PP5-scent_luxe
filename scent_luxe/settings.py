37e5929758e3196ac340be31cc1d353a8fe5f355
aimanh04 <aimanhawaemanoh04@gmail.com>
2025-04-18 17:05:09 +0200

    Webhook testing

--- scent_luxe/settings.py
+++ scent_luxe/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-5uy5e_t1%hhk##b_e!80_op+wl@u0-uogl3mk+8m2*s!#3+a*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', # local host
+    'localhost', # listen for stripe webhooks
+]
 
 
 # Application definition
@@ -183,6 +186,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
